da496379da771e3d0882a3a9355df4d15338f6df
MatteoB23 <anycos2323@gmail.com>
2025-12-12 09:48:02 +0100

    verbesserung Wettersimulator

--- SmartHome/WeatherSensor.cs
+++ SmartHome/WeatherSensor.cs
@@ -4,29 +4,39 @@ public class WeatherSensor
 {
     private readonly Random random = new();
     private int lastTemperature;
+    private int lastWindSpeed;
+    private bool isRaining = false;
     
     public WeatherSensor()
     {
         lastTemperature = random.Next(-5, 26);
     }
 
+    
     public WeatherData GenerateWeather()
     {
-        const int maxStep = 3;
-        int delta = random.Next(-maxStep, maxStep + 1);
-
-        // selten größere Sprünge (z.B. Wetterfront)
+        const int maxTempStep = 2;
+        const int maxWindStep = 7;
+        
+        int tempDelta = random.Next(-maxTempStep, maxTempStep + 1);
         if (random.NextDouble() < 0.02)
-            delta += random.Next(-5, 6);
+            tempDelta += random.Next(-5, 6);
 
-        int temp = Math.Clamp(lastTemperature + delta, -10, 35);
+        int temp = Math.Clamp(lastTemperature + tempDelta, -10, 35);
         lastTemperature = temp;
+        
+        int windDelta = random.Next(-maxWindStep, maxWindStep + 1);
+        int windSpeed = Math.Clamp(lastWindSpeed + windDelta, 0, 70);
+        lastWindSpeed = windSpeed;
+        
+        if (random.NextDouble() < 0.05)
+            isRaining = !isRaining;
 
         return new WeatherData
         {
             Temperature = temp,
-            WindSpeed = random.Next(0, 50),
-            IsRaining = random.Next(0, 2) == 1
+            WindSpeed = windSpeed,
+            IsRaining = isRaining
         };
     }
 }
