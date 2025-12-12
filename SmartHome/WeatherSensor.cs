1bf33b9c25cad6f56201777380114df02ed89d38
MatteoB23 <anycos2323@gmail.com>
2025-12-12 08:00:09 +0100

    Update Weathergenerierung

--- SmartHome/WeatherSensor.cs
+++ SmartHome/WeatherSensor.cs
@@ -2,13 +2,29 @@
 
 public class WeatherSensor
 {
-    private Random random = new Random();
+    private readonly Random random = new();
+    private int lastTemperature;
+    
+    public WeatherSensor()
+    {
+        lastTemperature = random.Next(-5, 26);
+    }
 
     public WeatherData GenerateWeather()
     {
+        const int maxStep = 3;
+        int delta = random.Next(-maxStep, maxStep + 1);
+
+        // selten größere Sprünge (z.B. Wetterfront)
+        if (random.NextDouble() < 0.02)
+            delta += random.Next(-5, 6);
+
+        int temp = Math.Clamp(lastTemperature + delta, -10, 35);
+        lastTemperature = temp;
+
         return new WeatherData
         {
-            Temperature = random.Next(-10, 36),
+            Temperature = temp,
             WindSpeed = random.Next(0, 50),
             IsRaining = random.Next(0, 2) == 1
         };
