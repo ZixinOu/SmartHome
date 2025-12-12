7460d66ddcf82a79102d265f27954aac01f1db30
MatteoB23 <anycos2323@gmail.com>
2025-12-12 15:17:54 +0100

    delete IsOccupied

--- SmartHome/Room.cs
+++ SmartHome/Room.cs
@@ -5,7 +5,6 @@ public abstract class Room
     public bool HasBlinds { get; protected set; }
     public bool HasAwnings { get; protected set; }
     public int TargetTemperature { get; set; } = 22;
-    public bool IsOccupied { get; protected set; }
 
     protected Room(string name, bool hasBlinds, bool hasAwnings = false)
     {
